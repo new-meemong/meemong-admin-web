6afcae0c365f1694d0fbd77296f3c9f431adb05d
Saehanseul <saehanseul@gmail.com>
2025-02-18 16:47:04 +0900

    feat: 테스트 도메인 추가

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
-const backURL = "https://meemong.com";
+// const backURL = "https://meemong.com";
+const backURL = "http://13.125.169.213:8088";
+
 // const backURL = "http://localhost:8088";
 export default backURL;
